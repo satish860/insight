805109095dca3fad3174c73ef9bac0576036f40e
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2013-08-10 11:07:49 +0530

    Integration of registration page

---
+++ Insight/Insight/App/viewmodels/registration/home.js
@@ -0,0 +1,26 @@
+﻿define(function (require) {
+    var router = require('durandal/plugins/router'),
+        index = require('viewmodels/registration/index'),
+    initapp = require('viewmodels/registration/initapp'),
+            detailPlan = require('viewmodels/registration/detailplan');
+    var valid = {
+        isInitAppValid: function () {
+            return (initapp.questions.errors().length > 0) ? 'Invalid' : 'Valid';
+        },
+        isDetailPlanValid: function () {
+            return (detailPlan.questions.errors().length > 0) ? 'Invalid' : 'Valid';
+        }
+    };
+    return {
+        router: router,
+        initapp: initapp,
+        valid: valid,
+        naivagateToInitApp: function () {
+            initapp.activate();
+            //return router.activate('registration/initapp');
+        },
+        activate: function () {
+            return router.activate('registration/detailplan');
+        }
+    };
+});
---
+++ Insight/Insight/App/viewmodels/registration/initapp.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../Scripts/knockout.validation.debug.js" />
+/// <reference path="../../Scripts/knockout-2.2.1.debug.js" />
+define(function (require) {
+    var http = require('durandal/http'),
+       system = require('durandal/system'),
+       app = require('durandal/app');
+
+    var questions =
+        {
+            QuestionCode1: 'QTS01',
+            Answer1: ko.observable().extend({ required: true }),
+            QuestionCode2: 'QTS02',
+            Answer2: ko.observable().extend({ required: true })
+
+        }
+
+    questions.errors = ko.validation.group(questions);
+
+    return {
+        questions: questions,
+        activate: function () {
+
+        }
+    }
+});
